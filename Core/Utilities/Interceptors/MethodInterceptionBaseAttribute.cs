1a8fef8851d10d320f8924cfcff5d1f239add57a
selçuk erbilgin <selcukerbilgin@gmail.com>
2021-10-30 20:36:13 +0300

    Autofac added Fluent api added AOP architecture implemented(got help from Engin Demirog' s repository)

---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace Core.Utilities.Interceptors
+{
+    // Autofac has Interceptor feature, default asp.net Ioc container doesn't.
+    //Inherited: attribute also works for the classes that inherits this one.
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+}
