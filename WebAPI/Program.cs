fabb0bc38a3950ca8cee7822049c096718d38420
selçuk erbilgin <selcukerbilgin@gmail.com>
2025-08-17 22:53:36 +0200

    Upgraded the project to .net 8 and removed startup file

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,33 +1,80 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Business.DependencyResolvers.Autofac;
-using Microsoft.AspNetCore.Hosting;
+using Core.DependencyResolver;
+using Core.Extensions;
+using Core.Utilities.IoC;
+using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.JWT;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
-namespace WebAPI
+var builder = WebApplication.CreateBuilder(args);
+
+// Autofac DI container
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>((context, containerBuilder) =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    containerBuilder.RegisterModule(new AutofacBusinessModule());
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureContainer<ContainerBuilder>(builder=> {
-                    builder.RegisterModule(new AutofacBusinessModule());
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddCors();
+builder.Services.AddDbContext<NorthWindContext>();
+
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = tokenOptions.Issuer,
+            ValidAudience = tokenOptions.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+        };
+    });
+
+builder.Services.AddDependencyResolvers(new ICoreModule[] {
+    new CoreModule()
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.EnvironmentName == "Development")
+{
+    app.UseDeveloperExceptionPage();
 }
+app.ConfigurecustomExceptionMiddleware();
+
+app.UseCors(policy => policy.WithOrigins("http://localhost:4200").AllowAnyHeader());
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Ensure database is migrated at startup
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<NorthWindContext>();
+    context.Database.Migrate();
+}
+
+app.Run();
